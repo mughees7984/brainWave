9d92d5051c331f782557e4801c6c90a5e3607315
Muhammad Mughees <mughees791@gmail.com>
2025-06-18 05:25:08 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./Components/Benefits";
+import Button from "./Components/Button";
+import Collaboration from "./Components/Collaboration";
+import Header from "./Components/Header";
+import Hero from "./Components/Hero";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits/>
+        <Collaboration/>
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
